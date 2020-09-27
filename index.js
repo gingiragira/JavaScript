b482c2e6b7644ce46631a09f0b280d531f77b499
gingiragira <71893954+gingiragira@users.noreply.github.com>
2020-09-27 18:37:51 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+//DOM
+const button = document.getElementById("addbtn");
+const lists = document.getElementById("lists");
+
+//関数（メソッド）
+function addlist(user) {
+  const list = document.createElement("li");
+  list.innerText =user.name;
+  lists.appendChild(list);
+}
+async function getUsers() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await res.json();
+  return users;
+}
+async function listUsers() {
+  const users = await getUsers();
+  users.forEach(addlist);
+}
+
+//イベント
+window.addEventListener("load", listUsers);
+button.addEventListener("click", listUsers);
+
+
+
+
+
+
+
+
+
